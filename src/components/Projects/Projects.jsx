a36cf988e6b58f6b901c8bbf113b8dae65116faa
gemmaordax <gemmaordax89@gmail.com>
2025-01-01 20:25:29 +0100

    Project Add

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,91 +1,102 @@
-/**
- * @file Projects.js
- * @description Componente que muestra una lista de proyectos con detalles interactivos. * 
- * @author Gemma Ordax
- */
-
 import React, { useState, useRef } from "react";
-import { FaGithub } from "react-icons/fa";
+import { FaCode } from "react-icons/fa";
+import { FaFigma } from "react-icons/fa";
 import projects from "../../data/projectsData";
 import "./Projects.css";
 
 function Projects() {
-    // Maneja el estado del proyecto seleccionado
-  const [selectedProject, setSelectedProject] = useState(null); 
-  const detailsRef = useRef(null);
+    const [selectedProject, setSelectedProject] = useState(null);
+    const detailsRef = useRef(null);
 
-    // Cambia el proyecto seleccionado y hace scroll a la sección de detalles
-  const handleProjectClick = (project) => {
-    setSelectedProject(project); 
-    setTimeout(() => {
-      detailsRef.current.scrollIntoView({ behavior: "smooth" }); 
-    }, 0);
-  };
+    const handleProjectClick = (project) => {
+        setSelectedProject(project);
+        setTimeout(() => {
+            detailsRef.current.scrollIntoView({ behavior: "smooth" });
+        }, 0);
+    };
 
-  return (
-    <section className="projects" ref={detailsRef}>
+    return (
+        <section className="projects" ref={detailsRef}>
             <h2>Projects</h2>
-      {/* Sección para mostrar detalles */}
-      <div
-        className="project-details-section"
-        style={{ display: selectedProject ? "flex" : "none" }} 
-      >
-        {selectedProject && (
-          <div className="project-details-content">
-            <div className="project-info">
-              <h2>{selectedProject.name}</h2>
-              <div className="project-description">
-                {selectedProject.description.split("\n").map((line, index) => (
-                  <p key={index}>{line}</p> 
+            <div
+                className="project-details-section"
+                style={{ display: selectedProject ? "flex" : "none" }}
+            >
+                {selectedProject && (
+                    <div className="project-details-content">
+                        <div className="project-info">
+                            <h2>{selectedProject.name}</h2>
+                            <div className="project-description">
+                                {selectedProject.description.split("\n").map((line, index) => (
+                                    <p key={index}>{line}</p>
+                                ))}
+                            </div>
+                            {/* Section for Technologies Used */}
+                            <div className="project-technologies">
+                                <h3>Technologies Used:</h3>
+                                <ul>
+                                    {selectedProject.technologies && selectedProject.technologies.map((tech, index) => (
+                                        <li key={index}>{tech}</li>
+                                    ))}
+                                </ul>
+                            </div>
+                            <div className="project-buttons">
+                                {selectedProject.liveDemoLink && (
+                                    <a
+                                        href={selectedProject.liveDemoLink}
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                        className="live-demo-btn"
+                                    >
+                                        <span>LIVE DEMO</span>
+                                    </a>
+                                )}
+                                {selectedProject.githubLink && (
+                                    <a
+                                        href={selectedProject.githubLink}
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                        className="github-btn"
+                                    >
+                                        <FaCode />
+                                    </a>
+                                )}
+                                {selectedProject.figmaLink && (
+                                    <a
+                                        href={selectedProject.figmaLink}
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                        className="github-btn"
+                                    >
+                                        <FaFigma />
+                                    </a>
+                                )}
+                            </div>
+                        </div>
+                        <div className="project-mockup">
+                            <img src={selectedProject.mockup} alt={`${selectedProject.name} mockup`} />
+                        </div>
+                    </div>
+                )}
+            </div>
+            <div className="projects-container">
+                {projects.map((project) => (
+                    <div
+                        className="project-card"
+                        key={project.id}
+                        onClick={() => handleProjectClick(project)}
+                    >
+                        <div className="project-image">
+                            <img src={project.image} alt={project.name} />
+                        </div>
+                        <div className="project-details">
+                            <h3>{project.name}</h3>
+                        </div>
+                    </div>
                 ))}
-              </div>
-              <div className="project-buttons">
-                <a
-                  href={selectedProject.liveDemoLink}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="live-demo-btn"
-                >
-                  <span>LIVE DEMO</span>
-                </a>
-                <a
-                  href={selectedProject.githubLink}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="github-btn"
-                >
-                  <FaGithub />
-                </a>
-              </div>
             </div>
-            <div className="project-mockup">
-              <img src={selectedProject.mockup} alt={`${selectedProject.name} mockup`} />
-            </div>
-          </div>
-        )}
-      </div>
-
-      {/* Sección de tarjetas */}
-
-      <div className="projects-container">
-        {projects.map((project) => (
-          <div
-            className="project-card"
-            key={project.id}
-            onClick={() => handleProjectClick(project)} 
-          >
-            <div className="project-image">
-              <img src={project.image} alt={project.name} />
-            </div>
-            <div className="project-details">
-              <h3>{project.name}</h3>
-
-            </div>
-          </div>
-        ))}
-      </div>
-    </section>
-  );
+        </section>
+    );
 }
 
 export default Projects;
--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,55 +1,76 @@
-
 //PROJECT IMAGES
 import project1Image from "../assets/projects/project1_h.png";
-import project2Image from "../assets/projects/project2_h.png";
+import project2Image from "../assets/projects/project2_h.jpeg";
 import project3Image from "../assets/projects/project3_h.png";
-import project4Image from "../assets/projects/project4_h.jpeg";
+import project4Image from "../assets/projects/project4_h.png";
+import project5Image from "../assets/projects/project5_h.png";
 
 //MOCKUP IMAGES
 import mockup1Image from "../assets/projects/mockup1.png";
 import mockup2Image from "../assets/projects/mockup2.png";
 import mockup3Image from "../assets/projects/mockup3.png";
 import mockup4Image from "../assets/projects/mockup4.png";
-
+import mockup5Image from "../assets/projects/mockup5.png";
 
 const projects = [
-    {
-      id: 1,
-      name: "Ion Park",
-      image: project1Image, 
-      liveDemoLink: "https://gemmaordax.github.io/ion_park/", 
-      githubLink: "https://github.com/gemmaordax/ion_park",
-      mockup: mockup1Image,
-      description: "This project is a visual development proposal for a web application called iPark. The website is in the development phase and is not yet finished, but it presents a basic and functional structure of the application.\nThe application includes several pages, some of which are hidden, such as the /payment route, which are still under development and not visible in the current version. These pages are intended for payment functionality, but are not yet implemented.",
-    },
-    {
-      id: 2,
-      name: "Weather App",
-      image: project4Image ,
-      liveDemoLink: "https://gemmaordax.github.io/weather_app/", 
-      githubLink: "https://github.com/gemmaordax/weather_app", 
-      mockup: mockup4Image,
-      description: "This weather application allows users to check the current weather based on their location or by manually searching for a city. It uses the OpenWeatherMap API to fetch weather data and the Unsplash API to display a background image of the city.\nThe app includes features such as temperature, humidity, wind speed, and feels-like temperature. It also handles geolocation, displaying the weather for the user's current location by default.",    },
-    {
-      id: 3,
-      name: "Escape Room",
-      image: project2Image ,
-      liveDemoLink: "https://gemmaordax.github.io/escape_room/", 
-      githubLink: "https://github.com/gemmaordax/escape_room", 
-      mockup: mockup2Image,
-      description: "This project is a practical exercise designed to apply basic tools of web design, frontend development, and databases. Its goal is to reinforce fundamental knowledge in programming and design.\nThe project consists of a platform where users must register to access the game. Once registered, they can participate in the interactive game, which combines logic and entertainment elements.\nIt is a simple yet complete example of how to develop a basic web application with dynamic and visual elements.",
-    },
-    {
-      id: 3,
-      name: "Mini Game",
-      image: project3Image , 
-      liveDemoLink: "https://gemmaordax.github.io/mini_game/", 
-      githubLink: "https://github.com/gemmaordax/mini_game", 
-      mockup: mockup3Image,
-      description: "This project is a practical exercise developed during my programming studies. The main objective is to implement a movement system for a character within a grid, using JavaScript, DOM, and events. Additionally, it includes extra features such as obstacle detection, coin collection, and portals.",
-    },
-    // Agrega más proyectos aquí
-  ];
-  
-  export default projects;
-  
+  {
+    id: 1,
+    name: "Ion Park",
+    image: project1Image,
+    liveDemoLink: "https://gemmaordax.github.io/ion_park/",
+    githubLink: "https://github.com/gemmaordax/ion_park",
+    mockup: mockup1Image,
+    description:
+      "This project is a visual development proposal for a web application called iPark. The website is in the development phase and is not yet finished, but it presents a basic and functional structure of the application.\nThe application includes several pages, some of which are hidden, such as the /payment route, which are still under development and not visible in the current version. These pages are intended for payment functionality, but are not yet implemented.",
+    technologies: ["React", "CSS", "Slack"],
+  },
+  {
+    id: 2,
+    name: "Weather App",
+    image: project2Image,
+    liveDemoLink: "https://gemmaordax.github.io/weather_app/",
+    githubLink: "https://github.com/gemmaordax/weather_app",
+    mockup: mockup2Image,
+    description:
+      "This weather application allows users to check the current weather based on their location or by manually searching for a city. It uses the OpenWeatherMap API to fetch weather data and the Unsplash API to display a background image of the city.\nThe app includes features such as temperature, humidity, wind speed, and feels-like temperature. It also handles geolocation, displaying the weather for the user's current location by default.",
+    technologies: ["React", "CSS"],
+  },
+  {
+    id: 3,
+    name: "Figma Parallax",
+    image: project3Image,
+    liveDemoLink:
+      "https://www.figma.com/proto/g5uxtfAFhmuC1wjG3NzmIZ/Projects?page-id=0%3A1&node-id=2800-197&viewport=779%2C80%2C0.07&t=2xw7AL3OgnOQZ3dl-1&scaling=scale-down&content-scaling=fixed&starting-point-node-id=2800%3A197",
+    figmaLink:
+      "https://www.figma.com/design/g5uxtfAFhmuC1wjG3NzmIZ/Projects?node-id=0-1&t=Gxxa0WdJihWBUHb1-1",
+    mockup: mockup3Image,
+    description:
+      "This project is a practical exercise designed to apply basic tools of web design, frontend development, and databases. Its goal is to reinforce fundamental knowledge in programming and design.\nThe project consists of a platform where users must register to access the game. Once registered, they can participate in the interactive game, which combines logic and entertainment elements.\nIt is a simple yet complete example of how to develop a basic web application with dynamic and visual elements.",
+    technologies: ["Figma"],
+  },
+
+  {
+    id: 4,
+    name: "Escape Room",
+    image: project4Image,
+    liveDemoLink: "https://gemmaordax.github.io/escape_room/",
+    githubLink: "https://github.com/gemmaordax/escape_room",
+    mockup: mockup4Image,
+    description:
+      "This project is a practical exercise designed to apply basic tools of web design, frontend development, and databases. Its goal is to reinforce fundamental knowledge in programming and design.\nThe project consists of a platform where users must register to access the game. Once registered, they can participate in the interactive game, which combines logic and entertainment elements.\nIt is a simple yet complete example of how to develop a basic web application with dynamic and visual elements.",
+    technologies: ["JavaScript", "CSS", "Firebase", "Jira"],
+  },
+  {
+    id: 5,
+    name: "Mini Game",
+    image: project5Image,
+    liveDemoLink: "https://gemmaordax.github.io/mini_game/",
+    githubLink: "https://github.com/gemmaordax/mini_game",
+    mockup: mockup5Image,
+    description:
+      "This project is a practical exercise developed during my programming studies. The main objective is to implement a movement system for a character within a grid, using JavaScript, DOM, and events. Additionally, it includes extra features such as obstacle detection, coin collection, and portals.",
+    technologies: ["JavaScript", "CSS"],
+  },
+];
+
+export default projects;
